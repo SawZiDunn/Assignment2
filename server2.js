c0ed69b0ab603c39bb2e434adca2b7b3059899b2
SawZiDunn <113846297+SawZiDunn@users.noreply.github.com>
2022-09-28 11:29:41 +0700

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,32 +1,25 @@
-const express= require("express");
+const express = require('express'); 
+const path = require('path');
+
 const app = express()
 
 app.get("",(req,res)=>{
 
-    res.send("<h1><center>Creating Home Page using express server through node.js</center></h1>")
-})
-
-app.get("/page1",(req,res)=>{
-
-    res.send("<h1><center>This is page 1.</center></h2>")
+    res.sendfile(path.join(__dirname+'/page1.html'));
 })
 
 app.get("/page2",(req,res)=>{
 
-    res.send("<h1><center>This is page 2.</center></h2>")
+    res.sendfile(path.join(__dirname+'/page2.html'));
+
 })
 
 app.get("/page3",(req,res)=>{
 
-    res.send("<h1><center>This is page 3.</center></h2>")
-})
-
-app.get("/page4",(req,res)=>{
-
-    res.send("<h1><center>This is page 4.</center></h2>")
+    res.sendfile(path.join(__dirname+'/page3.html'));
 })
 
 app.listen(3000,()=>{
 
-    console.log("Server is starting up at Port:3000");
+    console.log('Server starting up at: port:3000:');
 })
